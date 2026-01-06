a347efe09d1b1fd7ac59fa0bb350e03e6adc0aa5
krysmiller01 <krysmiller01@gmail.com>
2026-01-06 12:42:40 -0600

    B: added player.py and circleshape.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
     while True:
             log_state()
             for event in pygame.event.get():
@@ -15,5 +17,6 @@ def main():
                     return
             screen.fill("black")
             pygame.display.flip()
+            dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
