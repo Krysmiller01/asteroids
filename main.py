423d4e077c8716520ba4912d8c505019ce33f5c0
krysmiller01 <krysmiller01@gmail.com>
2026-01-08 13:38:35 -0600

    D: added collision and asteroid field.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import log_event
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -12,25 +17,36 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     
     while True:
-            log_state()
-            for event in pygame.event.get():
+          log_state()
+          for event in pygame.event.get():
                  if event.type == pygame.QUIT:
                     return
-            screen.fill("black")
+          screen.fill("black")
 
-            for sprite in updatable:
+          for sprite in updatable:
                  sprite.update(dt)
+          
+          for asteroid in asteroids:
+               if asteroid.collides_with(player) == True:
+                    log_event("player_hit")
+                    print("Game over!")
+                    sys.exit()
 
-            for sprite in drawable:
+          for sprite in drawable:
                  sprite.draw(screen)
-        
-            pygame.display.flip()
-            dt = clock.tick(60) / 1000
+
+          pygame.display.flip()
+          
+          dt = clock.tick(60) / 1000
             
 if __name__ == "__main__":
     main()
